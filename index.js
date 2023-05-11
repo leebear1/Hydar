2cf38a5e1a66089d0c60df941a4a7e8c21a103f7
leebear1 <129752371+leebear1@users.noreply.github.com>
2023-05-11 09:33:33 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const { Client, IntentsBitField} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildEmojisAndStickers,
+    ],
+})
+
+client.on('ready', (c) => {
+    console.log('Skynuts has awoken')
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot){
+        return
+    }
+
+    if (message.content === 'ping'){
+        message.reply('pong');
+    }
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot){
+        return
+    }
+
+    if (message.content === 'meow'){
+        message.reply('darn furries');
+    }
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot){
+        return
+    }
+
+    if (message.content === 'hydar'){
+        message.react(message.guild.emojis.cache.get('1105768332566736916'))
+    }
+});
+
+client.login(process.env.wwww);
