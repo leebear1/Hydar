634971ab46538d2ef6bbea059b972ee45041bafc
leebear1 <129752371+leebear1@users.noreply.github.com>
2023-05-11 13:39:31 +0100

    Update index.js
    fixed online 24/7

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
 require('dotenv').config();
-const { Client, IntentsBitField} = require('discord.js');
+const { Client, IntentsBitField } = require('discord.js');
+
+const keepAlive = require("./server");
+keepAlive();
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-        IntentsBitField.Flags.GuildEmojisAndStickers,
-    ],
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.GuildEmojisAndStickers,
+  ],
 })
 
 client.on('ready', (c) => {
-    console.log('Hydar has awoken')
+  console.log('Hydar has awoken')
 });
 
 client.on('messageCreate', (message) => {
-    if (message.author.bot){
-        return
-    }
+  if (message.author.bot) {
+    return
+  }
 
-    if (message.content === 'ping'){
-        message.reply('pong');
-    }
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
 });
 
 client.on('messageCreate', (message) => {
-    if (message.author.bot){
-        return
-    }
+  if (message.author.bot) {
+    return
+  }
 
-    if (message.content === 'meow'){
-        message.reply('darn furries');
-    }
+  if (message.content === 'meow') {
+    message.reply('darn furries');
+  }
 });
 
 client.on('messageCreate', (message) => {
-    if (message.author.bot){
-        return
-    }
+  if (message.author.bot) {
+    return
+  }
 
-    if (message.content === 'hydar'){        
-        message.react('<:hydar:1105768332566736916>')        
-    }
+  if (message.content === 'hydar') {
+    message.react('<:hydar:1105768332566736916>')
+  }
 });
 
 client.login(process.env.wwww);
